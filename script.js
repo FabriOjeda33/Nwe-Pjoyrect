a871f7296d415adc8dc7f5b5e23557f707e54f83
FabriOjeda33 <fabriojeda33@gmail.com>
2026-05-08 00:23:36 -0500

    Estoy haciendo la calculadora funcional, aun faltan cosas

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ BotonInicioCalculadora.addEventListener("click", () => {
   Calculator.style.display = "block";
 
 });
+const EnPantalla = document.getElementById("PantallaCalculadora");
+const BotonesNumeros = document.querySelectorAll(".numero");
+const Operadores = document.querySelectorAll(".operador");
+  BotonesNumeros.forEach((boton) => {
+    boton.addEventListener("click", () =>{
+      EnPantalla.value += boton.textContent;
+    });
+   });
+  Operadores.forEach((operador) => {
+  operador.addEventListener("click", () => {
+  EnPantalla.value += operador.textContent;
+  })
+ })
