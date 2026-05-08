ecaac184ae36ce899b1cae70971916f2443e55b1
FabriOjeda33 <fabriojeda33@gmail.com>
2026-05-07 23:28:42 -0500

    agregué una calculadora jaja

---
+++ script.js
@@ -0,0 +1,6 @@
+const BotonInicioCalculadora = document.getElementById("StartButton");
+const Calculator = document.getElementById("Calculator");
+BotonInicioCalculadora.addEventListener("click", () => {
+  Calculator.style.display = "block";
+
+});
